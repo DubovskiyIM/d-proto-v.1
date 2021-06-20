329d4d1576f9fa3780f6a3421b69a802f3209025
dehow <dehow@mail.ru>
2021-06-20 18:45:37 +0300

    create messages component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { EditComponent } from './lk/edit/edit.component';
 import { MyOrdersComponent } from './lk/my-orders/my-orders.component';
-import { LkComponent } from './lk/lk.component'; // CLI imports router
+import { LkComponent } from './lk/lk.component';
+import { MessagesComponent } from './lk/messages/messages.component'; // CLI imports router
 
 const lkRoutes: Routes = [
   { path: 'edit', component: EditComponent },
   { path: 'orders', component: MyOrdersComponent },
+  { path: 'messages', component: MessagesComponent },
 ];
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
