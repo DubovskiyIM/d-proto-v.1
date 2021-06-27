d36a189e07fa3298815a2ca047a9533f298791cc
dehow <dehow@mail.ru>
2021-06-27 14:29:25 +0300

    add chat

--- frontend/src/app/lk/chat/chat-message/chat-message.component.ts
+++ frontend/src/app/lk/chat/chat-message/chat-message.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AuthenticationService } from '../../../_services/authentication.service';
+import { IMessages } from '../../../_interfaces/chat';
 
 @Component({
   selector: 'app-chat-message',
   templateUrl: './chat-message.component.html',
-  styleUrls: ['./chat-message.component.scss']
+  styleUrls: ['./chat-message.component.scss'],
 })
-export class ChatMessageComponent implements OnInit {
+export class ChatMessageComponent {
+  @Input() messages: IMessages[] = [];
 
-  constructor() { }
+  private currentUser;
 
-  ngOnInit(): void {
+  constructor(private auth: AuthenticationService) {
+    this.auth.currentUser.subscribe((res) => {
+      this.currentUser = res;
+    });
   }
 
+  public isMyMessage(id: number): boolean {
+    return this.messages[id]?.data?.author?._id === this.currentUser?._id;
+  }
+
+  public isLastMessageInGroup(
+    indexGroup: number,
+    indexMessage: number
+  ): boolean {
+    const currentMessages = this.messages[indexGroup]?.data.messages;
+    return (
+      currentMessages.indexOf(currentMessages[indexMessage]) + 1 ===
+      currentMessages.length
+    );
+  }
 }
--- frontend/src/app/lk/chat/chat.component.ts
+++ frontend/src/app/lk/chat/chat.component.ts
@@ -1,35 +1,109 @@
-import { Component, OnInit } from '@angular/core';
-
-interface IMessage {
-  text: string;
-  time: string;
-  author: {
-    userId?: string;
-    name: string;
-  };
-}
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ChatService } from '../../_services/chat.service';
+import { IMessages } from '../../_interfaces/chat';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements AfterViewInit {
+  @ViewChild('footerChat', { read: ElementRef, static: false })
+  footerChatView: ElementRef;
+  public footerChatHeight;
   selectedConversation = {
     user_name: 'Carl',
   };
 
-  messages: IMessage[] = [];
+  public messageForm: FormGroup = new FormGroup({
+    message: new FormControl(''),
+  });
 
-  constructor() {}
+  public messages: IMessages[] = [
+    {
+      id: '2213321',
+      data: {
+        author: {
+          name: 'Cris',
+          _id: '60b61a4655abc60079d026d3',
+        },
+        messages: [
+          {
+            content: 'Трусы еще в наличии, хорошее ли состояние?',
+            time: Date.now(),
+            type: 'text',
+          },
+          {
+            content: 'Есть ли запах?',
+            time: Date.now(),
+            type: 'text',
+          },
+          {
+            content: 'готов заказать',
+            time: Date.now(),
+            type: 'text',
+          },
+        ],
+      },
+    },
+    {
+      id: '123123123123',
+      data: {
+        author: {
+          name: 'Cyle',
+          _id: '60b61a4655a231279d026d3',
+        },
+        messages: [
+          {
+            content: 'К сожалению их уже забрали',
+            time: Date.now(),
+            type: 'text',
+          },
+          {
+            content: 'Но скоро будет поступление',
+            time: Date.now(),
+            type: 'text',
+          },
+        ],
+      },
+    },
+  ];
 
-  ngOnInit(): void {
+  constructor(private chatService: ChatService) {}
+
+  ngAfterViewInit(): void {
+    this.footerChatHeight = this.footerChatView.nativeElement.offsetHeight;
+    console.log(this.footerChatHeight);
+  }
+
+  public sendMessage(): void {
+    if (!this.messageForm.controls.message.value) {
+      return;
+    }
+    this.chatService.sendMessage(this.messageForm.controls);
     this.messages.push({
-      text: '',
-      time: '10:23',
-      author: {
-        name: 'Ignat Dubovsky',
+      id: Math.round(100000).toString(),
+      data: {
+        author: {
+          name: 'Cris',
+          _id: '60b61a4655abc60079d026d3',
+        },
+        messages: [
+          {
+            content: this.messageForm.controls.message.value,
+            time: Date.now(),
+            type: 'text',
+          },
+        ],
       },
     });
+    this.messageForm.reset();
   }
 }
