95c163aaabc2d1cbbb120cbe3842f2a787ba49ad
Ignat Dubovskiy <qwor50@gmail.com>
2021-06-29 22:15:29 +0300

    #V2Back. Create rooms controller and service.

--- backend/src/modules/rooms/rooms.controller.ts
+++ backend/src/modules/rooms/rooms.controller.ts
@@ -1,34 +1,70 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body, Request,
+  Controller,
+  Get, Post, Put, Delete,
+  HttpException, HttpStatus
+} from '@nestjs/common';
+
 import { RoomsService } from './rooms.service';
-import { CreateRoomDto } from './dto/create-room.dto';
-import { UpdateRoomDto } from './dto/update-room.dto';
+import { Room } from '../../types/room';
 
 @Controller('rooms')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
 
-  @Post()
-  create(@Body() createRoomDto: CreateRoomDto) {
-    return this.roomsService.create(createRoomDto);
-  }
-
   @Get()
-  findAll() {
-    return this.roomsService.findAll();
+  async index(): Promise<Room[]> {
+    return await this.roomsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.roomsService.findOne(+id);
+  async show(@Request() req): Promise<Room> {
+    const id = req.params.id;
+    if (!id)
+      throw new HttpException(
+          'ID parameter is missing',
+          HttpStatus.BAD_REQUEST,
+      );
+
+    const room = await this.roomsService.findById(id);
+    if (!room)
+      throw new HttpException(
+          `The room with the id: ${id} does not exists`,
+          HttpStatus.BAD_REQUEST,
+      );
+
+    return room;
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {
-    return this.roomsService.update(+id, updateRoomDto);
+  @Post()
+  async create(@Body() body: Room): Promise<Room> {
+    if (!body || (body && Object.keys(body).length === 0))
+      throw new HttpException('Missing information', HttpStatus.BAD_REQUEST);
+
+    return await this.roomsService.create(body);
+  }
+
+  @Put(':id')
+  async update(@Request() req) {
+    const id = req.params.id;
+    if (!id)
+      throw new HttpException(
+          'ID parameter is missing',
+          HttpStatus.BAD_REQUEST,
+      );
+
+    await this.roomsService.update(id, req.body);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.roomsService.remove(+id);
+  public async delete(@Request() req) {
+    const id = req.params.id;
+    if (!id)
+      throw new HttpException(
+          'ID parameter is missing',
+          HttpStatus.BAD_REQUEST,
+      );
+
+    await this.roomsService.delete(id);
   }
 }
