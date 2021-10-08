2b54220e0eed57226f19d368689f90e62fed76af
Ignat Dubovskiy <idubovskiy@dc2b.ru>
2021-10-08 17:32:28 +0300

    #V2. Implement search module and logic.

--- backend/src/modules/rooms/rooms.module.ts
+++ backend/src/modules/rooms/rooms.module.ts
@@ -4,20 +4,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RoomsController } from './rooms.controller';
 import { RoomsService } from './rooms.service';
 import { RoomSchema } from '@src/models/room.schema';
-import { MessageSchema } from "@src/models/message.schema";
+import { MessageSchema } from '@src/models/message.schema';
 import { ChatService } from '@src/modules/chat/chat.service';
-import { UserSchema } from "@src/models/user.schema";
-import { UsersService } from "@src/modules/users/users.service";
+import { UserSchema } from '@src/models/user.schema';
+import { UsersService } from '@src/modules/users/users.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Room', schema: RoomSchema }]),
     MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
-      MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
   controllers: [RoomsController],
   providers: [RoomsService, ChatService, UsersService],
   exports: [RoomsService],
 })
-export class RoomsModule {
-}
+export class RoomsModule {}
