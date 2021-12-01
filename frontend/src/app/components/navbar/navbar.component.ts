34e4969775a8c73ed43e814ef6aa4832ceaae735
dehow <dehow@mail.ru>
2021-12-01 20:45:32 +0300

    update navbar

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { User } from '../../_interfaces/user';
 import { StarRatingColor } from '../star-ratings/star-ratings.component';
 import { AuthenticationService } from '../../_services/authentication.service';
-import {NavigationService} from "../../_services/navigation.service";
+import { NavigationService } from '../../_services/navigation.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,15 +11,25 @@ import {NavigationService} from "../../_services/navigation.service";
 })
 export class NavbarComponent implements OnInit {
   public rating = 4.4;
+
   public isLogged: true;
   public user: User;
+  isOpenedMenu = false;
+  @Input() navLinks: {
+    link: string;
+    label: string;
+  }[];
+
+  @ViewChild('sideBarMenu', { static: false })
+  sideBarMenu: ElementRef;
+
   public starColor: StarRatingColor = StarRatingColor.accent;
 
   public starColorP: StarRatingColor = StarRatingColor.primary;
 
   public starColorW: StarRatingColor = StarRatingColor.warn;
 
-  constructor(private auth: AuthenticationService, private navigateService: NavigationService) {}
+  constructor(private auth: AuthenticationService, private navigateService: NavigationService, private renderer: Renderer2) {}
 
   ngOnInit() {
     this.auth.currentUser.subscribe((res) => {
@@ -28,10 +38,22 @@ export class NavbarComponent implements OnInit {
         this.user = res;
       }
     });
+
+    console.log('NAV-LINKS', this.navLinks);
   }
 
   onRatingChanged(rating) {
     this.rating = rating;
+  }
+
+  public toggleNavbar(): void {
+    if (this.isOpenedMenu) {
+      this.renderer.addClass(this.sideBarMenu.nativeElement, 'close');
+    } else {
+      this.renderer.removeClass(this.sideBarMenu.nativeElement, 'close');
+    }
+
+    this.isOpenedMenu = !this.isOpenedMenu;
   }
 
   public goHomePage() {
